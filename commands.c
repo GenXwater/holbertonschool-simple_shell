f113b5521f4bd6554a0723df2dfecd4600589ed1
GenXwater <louis.genty@outlook.com>
2024-08-20 11:29:12 +0200

    Refactored command execution and argument splitting logic

--- commands.c
+++ commands.c
@@ -5,9 +5,6 @@
  * @str: The string to split
  * @argv: The array to store the words
  * @max_args: Maximum number of arguments
- *
- * Description: This function splits the input string into words,
- * but in this context, it is designed to only accept one word.
  */
 void split_string_to_av(char *str, char *argv[], int max_args)
 {
@@ -15,7 +12,7 @@ void split_string_to_av(char *str, char *argv[], int max_args)
 	char *token;
 
 	token = strtok(str, " \t\n");
-	while (token && i < max_args)
+	while (token && i < max_args - 1)
 	{
 		argv[i] = token;
 		i++;
@@ -28,21 +25,11 @@ void split_string_to_av(char *str, char *argv[], int max_args)
  * execute_command - Executes a command with its arguments
  * @argv: Array of command and arguments
  * @envp: Environment variables
- *
- * Description: This function forks a process and executes a command using
- * execve. The parent process waits for the child to finish.
  */
 void execute_command(char *argv[], char *envp[])
 {
 	pid_t pid;
 	int status;
-
-	/* Check if command is an absolute path */
-	if (argv[0][0] != '/')
-	{
-		fprintf(stderr, "%s: Command not found\n", argv[0]);
-		return;
-	}
 
 	pid = fork();
 	if (pid == -1)
@@ -63,4 +50,48 @@ void execute_command(char *argv[], char *envp[])
 	{
 		wait(&status);
 	}
+}
+
+/**
+ * handle_builtin_commands - Handles the built-in commands for the shell
+ * @cmd_argv: Array of command arguments
+ * @envp: Environment variables
+ *
+ * Return: 1 if a built-in command was executed, 0 otherwise
+ */
+int handle_builtin_commands(char *cmd_argv[], char *envp[])
+{
+	if (strcmp(cmd_argv[0], "exit") == 0)
+	{
+		exit(0);
+	}
+	else if (strcmp(cmd_argv[0], "printenv") == 0)
+	{
+		print_environment(envp);
+		return (1);
+	}
+	else if (strcmp(cmd_argv[0], "setenv") == 0 && cmd_argv[1] && cmd_argv[2])
+	{
+		_setenv(cmd_argv[1], cmd_argv[2], 1, envp);
+		return (1);
+	}
+	else if (strcmp(cmd_argv[0], "unsetenv") == 0 && cmd_argv[1])
+	{
+		_unsetenv(cmd_argv[1], envp);
+		return (1);
+	}
+	else if (strcmp(cmd_argv[0], "showpath") == 0)
+	{
+		print_path_directories(envp);
+		return (1);
+	}
+	else if (strcmp(cmd_argv[0], "man") == 0 && cmd_argv[1] &&
+			strcmp(cmd_argv[1], "simple_shell") == 0)
+	{
+		char *man_command[] = {"/bin/man", "./simple-shell.1", NULL};
+
+		execute_man_command(man_command);
+		return (1);
+	}
+	return (0);
 }
