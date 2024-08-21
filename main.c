3976e5c1ed0b727732110ac6f80fec7fd0a003a2
GenXwater <louis.genty@outlook.com>
2024-08-21 18:38:08 +0200

    Fix memory management for line in main

--- main.c
+++ main.c
@@ -1,6 +1,22 @@
 #include "main.h"
 
 /**
+ * string_length - Calculates the length of a string
+ * @str: The string to measure
+ *
+ * Return: The length of the string
+ */
+int string_length(char *str)
+{
+	int len = 0;
+
+	while (str[len] != '\0')
+		len++;
+
+	return (len);
+}
+
+/**
  * main - Entry point of the simple shell
  * @argc: Argument count
  * @argv: Argument vector
@@ -15,13 +31,14 @@ int main(int argc, char *argv[], char *envp[])
 	ssize_t nread;
 	char *cmd_argv[10];
 	int max_args = 10;
+	int interactive = isatty(0);
 
 	(void)argc;  /* Marking unused parameters */
 	(void)argv;
 
 	while (1)
 	{
-		if (isatty(STDIN_FILENO))
+		if (interactive)
 			printf("$ ");
 		nread = getline(&line, &len, stdin);
 		if (nread == -1)
@@ -30,7 +47,9 @@ int main(int argc, char *argv[], char *envp[])
 			break; /* Handle EOF (Ctrl+D) */
 		}
 
-		line[strcspn(line, "\n")] = 0;  /* Remove the newline character */
+		/* Remove the newline character manually */
+		if (line[nread - 1] == '\n')
+			line[nread - 1] = '\0';
 
 		split_string_to_av(line, cmd_argv, max_args);
 
@@ -46,3 +65,4 @@ int main(int argc, char *argv[], char *envp[])
 	free(line);
 	return (0);
 }
+
